8ea45d5f2b541389432d8e27ab1deb293ed2c237
AnbuSivaji <anbusoft57@gmail.com>
2025-04-04 16:30:09 +0530

    Adding Instruction

--- ReverseString.java
+++ ReverseString.java
@@ -6,7 +6,18 @@ public class ReverseString {
         String A=sc.next();
         String B="";
         
-        for(int i=A.length()-1;i>=0;i--){
+        for(int i=A.length()-1;i>=0;i--)
+        
+        
+        { // Loop to reverse the string
+            // A.charAt(i) returns the character at index i of string A
+            // B=B+A.charAt(i) appends the character to string B
+            // So, B will be the reverse of A after the loop ends
+            // For example, if A is "hello", B will be "olleh" after the loop
+            // ends
+            // This is a common way to reverse a string in Java using a loop
+
+
             B=B+A.charAt(i);
         }
         if(A.equals(B)){
