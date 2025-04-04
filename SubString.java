88846989a4099931d4263c7ef76f767ae18a313f
AnbuSivaji <anbusoft57@gmail.com>
2025-04-04 17:51:40 +0530

    No

---
+++ SubString.java
@@ -0,0 +1,12 @@
+
+
+public class SubString {
+    public  static void main(String args[]){
+        String A="Hello";
+        System.out.println("Original String :"+A);
+
+        String B=A.substring(0,2 );
+        System.out.println("SubString :"+B);
+
+    }
+}
