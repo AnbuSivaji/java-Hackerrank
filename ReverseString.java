6f7bc1cc4086162afe8b002c8bad389e77756c3b
AnbuSivaji <anbusoft57@gmail.com>
2025-04-04 16:28:06 +0530

    Reverse String

---
+++ ReverseString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseString {
+       public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B="";
+        
+        for(int i=A.length()-1;i>=0;i--){
+            B=B+A.charAt(i);
+        }
+        if(A.equals(B)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+
+       }
+}
